d8bb65a78a43c0a97856f66c2904af7e4b5b063e
CoderXDMa <derrickmalove@163.com>
2026-04-30 15:23:34 +0800

    update: 更新 news-index.json 添加 20260430 新闻，刷新 SW 缓存版本

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v1';
+const CACHE_VERSION  = 'v20260430-152244';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
