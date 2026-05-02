3bf1269c451bff1db1c6ae51bb60a3d529aa8289
CoderXDMa <derrickmalove@163.com>
2026-05-02 12:08:34 +0800

    feat: 更新早间新闻时效性规则为昨日07:00至今日07:00
    - 明确新闻日期与搜索时段的对照规则
    - 新增指定日期生成新闻的说明
    - 将所有'24小时'表述统一为明确的时间段定义
    - 增加补漏生成的注意事项

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v20260502-012749';
+const CACHE_VERSION  = 'v20260502-120835';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
