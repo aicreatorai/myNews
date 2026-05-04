cebff2bb58a00a44c7f84e59500805f3fdd57f28
CoderXDMa <derrickmalove@163.com>
2026-05-04 10:00:02 +0800

    更新20260504谣言文件

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v20260504-095707';
+const CACHE_VERSION  = 'v20260504-100003';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
