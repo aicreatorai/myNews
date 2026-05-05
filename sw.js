800398dbad2352b0e64fa9c5606952b6c106ee63
CoderXDMa <derrickmalove@163.com>
2026-05-05 13:50:18 +0800

    添加2026-05-05早间新闻，更新索引和缓存版本

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v20260504-100003';
+const CACHE_VERSION  = 'v20260505-135018';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
