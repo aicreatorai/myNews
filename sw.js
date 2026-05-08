de02a98c2b7ee13b06baf73a26c4201d5058acb7
CoderXDMa <derrickmalove@163.com>
2026-05-08 09:23:06 +0800

    添加 2026-05-08 早间新闻素材

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v20260507-092043';
+const CACHE_VERSION  = 'v20260508-092306';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
