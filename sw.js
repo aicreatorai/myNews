2291c66fe13c0e188f195d2220c4ca3dae9006e0
CoderXDMa <derrickmalove@163.com>
2026-05-10 23:47:44 +0800

    新增 20260510 周报，更新早间新闻

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v20260510-122324';
+const CACHE_VERSION  = 'v20260510-234744';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
