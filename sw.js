3da85aa6dd83e9d210c4fd0ef5dfbe1ca0a2daec
CoderXDMa <derrickmalove@163.com>
2026-05-13 21:54:05 +0800

    chore: 同步 APP_VERSION 版本号

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const CACHE_VERSION  = 'v20260513-214954';
+const CACHE_VERSION  = 'v20260513-215405';
 const STATIC_CACHE   = 'news-static-' + CACHE_VERSION;
 const NEWS_CACHE     = 'news-content-' + CACHE_VERSION;
 const CDN_CACHE      = 'news-cdn-' + CACHE_VERSION;
