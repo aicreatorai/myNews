d188cd77697ea28ea3ec9b19c7ac3e8dc4d133b2
derrick <derrickmalove@163.com>
2026-07-26 07:21:08 +0800

    2026-07-26 早间新闻（12板块）

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
     'use strict';
 
     // --- 缓存版本号：修改此值会使所有 localStorage 缓存失效 ---
-    const CACHE_VERSION = 'v42';
+    const CACHE_VERSION = 'v43';
 
     // --- State ---
     let indexData = null;
